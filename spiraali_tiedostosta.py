4b4c4a8cdef36e63c5603be72a12eeb263d98aab
Teemu Mäenpää <teemumaen@gmail.com>
2020-10-30 19:09:51 +0200

    Korjaukset

--- spiraali_tiedostosta.py
+++ spiraali_tiedostosta.py
@@ -18,18 +18,19 @@ def piirra_tiedostosta(tiedosto):
         for j in range(len(spiraali[i])):
             if j == 0:
                 vari = spiraali[i][j]
-                vari = vari.strip()
+                #vari = vari.strip()
             if j == 1:
                 kaaret = int(spiraali[i][j])
-                kaaret = kaaret.strip()
+                #kaaret = kaaret.strip()
             if j == 2:
                 r = float(spiraali[i][j])
-                r = r.strip()
+                #r = r.strip()
             if j == 3:
                 r_kasvu = float(spiraali[i][j])
-                pisteet_2 = pisteet_2.strip()
+                #pisteet_2 = pisteet_2.strip()
             if j == 4:
                 viiva = int(spiraali[i][j])
-                viiva = viiva.strip
-            draw_spiral(vari, kaaret, r, r_kasvu, viiva)
+                #viiva = viiva.strip
+        draw_spiral(vari, kaaret, r, r_kasvu, viiva)
 piirra_tiedostosta('spiraali.txt')
+done()
